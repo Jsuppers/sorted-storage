182c2f6d4b168b373886cfd8c5e15f647af13d6c
Jsuppers <jorissuppers@gmail.com>
2021-04-13 00:29:04 +0300

    feature: back button in share dialog

--- lib/ui/widgets/share_widget.dart
+++ lib/ui/widgets/share_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,9 +15,9 @@ class ShareWidget extends StatefulWidget {
 
   // ignore: public_member_api_docs
   final String folderID;
+
   // ignore: public_member_api_docs
   final SharingState state;
-
 
   @override
   _ShareWidgetState createState() => _ShareWidgetState();
@@ -35,85 +34,81 @@ class _ShareWidgetState extends State<ShareWidget> {
       controller.text = '${Constants.websiteURL}/view/${widget.folderID}';
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        if (widget.state.errorMessage != null) Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Icon(Icons.error),
-            const SizedBox(width: 5),
-            Text(widget.state.errorMessage),
-          ],
-        ) else Container(),
-        if (shared) Container(
-          padding: const EdgeInsets.all(20),
-          width: 300,
-          child: Row(
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: CustomScrollView(slivers: <Widget>[
+        const SliverAppBar(
+          // toolbarHeight: 50,
+          floating: true,
+          backgroundColor: Colors.white,
+          pinned: true,
+          elevation: 0.0,
+        ),
+        SliverToBoxAdapter(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.copy, size: 20),
-                iconSize: 20,
-                splashRadius: 20,
-                padding: EdgeInsets.zero,
-                onPressed: () => Clipboard.setData(
-                    ClipboardData(text: controller.text)),
-              ),
-              const SizedBox(width: 10),
-              SizedBox(
-                width: 200,
-                child: TextField(
-                    controller: controller,
-                    style: myThemeData.textTheme.bodyText1,
-                    minLines: 2,
-                    maxLines: 4,
-                    readOnly: true),
-              )
-            ],
-          ),
-        ) else const Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Text(
-              'To make this event publicly visible click the share button.'),
-        ),
-        ShareButton(
-            key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-            shared: shared,
-            loading: false),
-        Container(
-          padding: const EdgeInsets.all(20),
-          child: shared
-              ? const Text(
-              'Everyone with this link can see and comment on your content. '
-                  'Be careful who you give it to!')
-              : Container(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              MaterialButton(
-                  minWidth: 100,
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  onPressed: () => BlocProvider.of<NavigationBloc>(context)
-                      .add(NavigatorPopEvent()),
+              if (widget.state.errorMessage != null)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Icon(Icons.error),
+                    const SizedBox(width: 5),
+                    Text(widget.state.errorMessage),
+                  ],
+                )
+              else
+                Container(),
+              if (shared)
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: 300,
                   child: Row(
-                    children: const <Widget>[
-                      Icon(
-                        Icons.cancel,
-                        color: Colors.black,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.copy, size: 20),
+                        iconSize: 20,
+                        splashRadius: 20,
+                        padding: EdgeInsets.zero,
+                        onPressed: () => Clipboard.setData(
+                            ClipboardData(text: controller.text)),
                       ),
-                      SizedBox(width: 5),
-                      Text('close'),
+                      const SizedBox(width: 10),
+                      SizedBox(
+                        width: 200,
+                        child: TextField(
+                            controller: controller,
+                            style: myThemeData.textTheme.bodyText1,
+                            minLines: 2,
+                            maxLines: 4,
+                            readOnly: true),
+                      )
                     ],
-                  )),
+                  ),
+                )
+              else
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text('To make this event publicly visible '
+                      'click the share button.'),
+                ),
+              ShareButton(
+                  key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                  shared: shared,
+                  loading: false),
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: shared
+                    ? const Text('Everyone with this link can see and comment '
+                        'on your content. '
+                        'Be careful who you give it to!')
+                    : Container(),
+              ),
             ],
           ),
-        ),
-      ],
+        )
+      ]),
     );
   }
 }
