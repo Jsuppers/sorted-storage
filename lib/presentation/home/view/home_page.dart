e2db158a9687165dd4cc072b60fd2b17f74ca85d
CoderUni <57564335+CoderUni@users.noreply.github.com>
2021-07-04 17:15:55 +0800

    feat: Added home page

--- lib/presentation/home/view/home_page.dart
+++ lib/presentation/home/view/home_page.dart
@@ -1,6 +1,12 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+
+// Project imports:
+import 'package:sorted_storage/layout/layout.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,9 +17,40 @@ class HomePage extends StatelessWidget {
         child: CustomScrollView(
           slivers: [
             SliverList(
-              delegate: SliverChildListDelegate([
-                const Center(child: Text('Landing Page')),
-              ]),
+              delegate: SliverChildListDelegate(
+                [
+                  SizedBox(height: AppSpacings.six),
+                  Row(
+                    children: [
+                      SizedBox(width: AppSpacings.twelve),
+                      InkWell(
+                        customBorder: const CircleBorder(),
+                        onTap: () => null,
+                        child: Padding(
+                          padding: EdgeInsets.all(AppSpacings.twelve),
+                          child: const Icon(EvaIcons.folderAdd),
+                        ),
+                      ),
+                      const Spacer(),
+                      InkWell(
+                        customBorder: const StadiumBorder(),
+                        onTap: () => null,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: AppSpacings.four,
+                            horizontal: AppSpacings.eight,
+                          ),
+                          child: Image.asset(
+                            'assets/images/logo_tiny.png',
+                            height: AppSpacings.fortyEight,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: AppSpacings.twelve),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
