c8edaa286813a908febfc663c8a69f499c4140c4
CoderUni <57564335+CoderUni@users.noreply.github.com>
2021-07-16 15:00:47 +0800

    test: added initial state test for landing bloc

--- test/presentation/landing/bloc/landing_bloc_test.dart
+++ test/presentation/landing/bloc/landing_bloc_test.dart
@@ -12,50 +12,57 @@ void main() {
     _landingNavigationBloc = LandingNavigationBloc();
   });
 
-  group('Floating Action Button', () {
-    blocTest(
-      'emits updated state',
-      build: () => _landingNavigationBloc,
-      act: (LandingNavigationBloc bloc) =>
-          bloc.add(const LandingNavigationFloatingActionButtonPressed()),
-      expect: () =>
-          [const LandingNavigationFloatingActionButtonToggledInProgress()],
-    );
-  });
+  group('LandingBloc', () {
+    test('initial state is page 0', () {
+      expect(_landingNavigationBloc.state,
+          const LandingNavigationPageChangeSuccess(0));
+    });
 
-  group('Page Selection', () {
-    blocTest(
-      'emits state with page 0 when home button is tapped',
-      build: () => _landingNavigationBloc,
-      act: (LandingNavigationBloc bloc) =>
-          bloc.add(const LandingNavigationHomeButtonPressed()),
-      expect: () => [const LandingNavigationPageChangeSuccess(0)],
-    );
+    group('Floating Action Button', () {
+      blocTest(
+        'emits updated state',
+        build: () => _landingNavigationBloc,
+        act: (LandingNavigationBloc bloc) =>
+            bloc.add(const LandingNavigationFloatingActionButtonPressed()),
+        expect: () =>
+            [const LandingNavigationFloatingActionButtonToggledInProgress()],
+      );
+    });
 
-    blocTest(
-      'emits state with page 1 if profile button is tapped',
-      build: () => _landingNavigationBloc,
-      act: (LandingNavigationBloc bloc) =>
-          bloc.add(const LandingNavigationProfileButtonPressed()),
-      expect: () => [const LandingNavigationPageChangeSuccess(1)],
-    );
+    group('Page Selection', () {
+      blocTest(
+        'emits state with page 0 when home button is tapped',
+        build: () => _landingNavigationBloc,
+        act: (LandingNavigationBloc bloc) =>
+            bloc.add(const LandingNavigationHomeButtonPressed()),
+        expect: () => [const LandingNavigationPageChangeSuccess(0)],
+      );
 
-    blocTest(
-      'emits state with page 2 if about button is tapped',
-      build: () => _landingNavigationBloc,
-      act: (LandingNavigationBloc bloc) =>
-          bloc.add(const LandingNavigationAboutButtonPressed()),
-      expect: () => [const LandingNavigationPageChangeSuccess(2)],
-    );
-  });
+      blocTest(
+        'emits state with page 1 if profile button is tapped',
+        build: () => _landingNavigationBloc,
+        act: (LandingNavigationBloc bloc) =>
+            bloc.add(const LandingNavigationProfileButtonPressed()),
+        expect: () => [const LandingNavigationPageChangeSuccess(1)],
+      );
 
-  group('Donation Button', () {
-    blocTest(
-      'emits state when donation button is tapped',
-      build: () => _landingNavigationBloc,
-      act: (LandingNavigationBloc bloc) =>
-          bloc.add(const LandingNavigationDonateButtonPressed()),
-      expect: () => [const LandingNavigationOpenDonationPageInProgress()],
-    );
+      blocTest(
+        'emits state with page 2 if about button is tapped',
+        build: () => _landingNavigationBloc,
+        act: (LandingNavigationBloc bloc) =>
+            bloc.add(const LandingNavigationAboutButtonPressed()),
+        expect: () => [const LandingNavigationPageChangeSuccess(2)],
+      );
+    });
+
+    group('Donation Button', () {
+      blocTest(
+        'emits state when donation button is tapped',
+        build: () => _landingNavigationBloc,
+        act: (LandingNavigationBloc bloc) =>
+            bloc.add(const LandingNavigationDonateButtonPressed()),
+        expect: () => [const LandingNavigationOpenDonationPageInProgress()],
+      );
+    });
   });
 }
